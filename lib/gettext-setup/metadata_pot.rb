613ce4b39ee9275415dc5f7cae20851b5281c5a1
Scott McClellan <scott.mcclellan@puppetlabs.com>
2017-04-11 03:54:59 -0500

    (maint) Refactoring for POT metadata tests
    This moves files around and adds tests for existing functionality. It
    includes some standardization of existing code.

---
+++ lib/gettext-setup/metadata_pot.rb
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+
+require 'erb'
+require 'json'
+
+module GettextSetup
+  module MetadataPot
+    def self.metadata_path
+      File.join(GettextSetup.locales_path, GettextSetup.config['project_name'] + '_metadata.pot')
+    end
+
+    def self.template_path
+      File.join(File.dirname(__FILE__), '../templates/metadata.pot.erb')
+    end
+
+    def self.metadata(metadata_file = 'metadata.json')
+      @metadata ||= begin
+        file = open(metadata_file)
+        json = file.read
+        JSON.parse(json)
+      end
+    end
+
+    def self.pot_string(metadata)
+      b = binding.freeze
+      # Uses `metadata`
+      ERB.new(File.read(template_path)).result(b)
+    end
+
+    def self.generate_metadata_pot(pot_metadata = metadata, path = metadata_path)
+      open(path, 'w') do |f|
+        f << pot_string(pot_metadata)
+      end
+    end
+  end
+end
--- spec/lib/pot_spec.rb
+++ spec/lib/pot_spec.rb
@@ -1,28 +1,28 @@
 require 'rspec/expectations'
 require_relative '../spec_helper.rb'
 
-require_relative '../../lib/tasks/task_helper.rb'
+require_relative '../../lib/gettext-setup/pot'
 
 describe 'string_changes?', if: msgcmp_present? do
   old_pot = File.absolute_path('../fixtures/pot_update/old.pot', File.dirname(__FILE__))
 
   it 'should detect string addition' do
     new_pot = File.absolute_path('../fixtures/pot_update/add.pot', File.dirname(__FILE__))
-    expect(string_changes?(old_pot, new_pot)).to eq(true)
+    expect(GettextSetup::Pot.string_changes?(old_pot, new_pot)).to eq(true)
   end
 
   it 'should detect string removal' do
     new_pot = File.absolute_path('../fixtures/pot_update/remove.pot', File.dirname(__FILE__))
-    expect(string_changes?(old_pot, new_pot)).to eq(true)
+    expect(GettextSetup::Pot.string_changes?(old_pot, new_pot)).to eq(true)
   end
 
   it 'should detect string changes' do
     new_pot = File.absolute_path('../fixtures/pot_update/change.pot', File.dirname(__FILE__))
-    expect(string_changes?(old_pot, new_pot)).to eq(true)
+    expect(GettextSetup::Pot.string_changes?(old_pot, new_pot)).to eq(true)
   end
 
   it 'should not detect non-string changes' do
     new_pot = File.absolute_path('../fixtures/pot_update/non_string_changes.pot', File.dirname(__FILE__))
-    expect(string_changes?(old_pot, new_pot)).to eq(false)
+    expect(GettextSetup::Pot.string_changes?(old_pot, new_pot)).to eq(false)
   end
 end
