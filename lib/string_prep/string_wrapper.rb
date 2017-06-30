12367616f5d296bc9407454a9711fe75b1a3cd7a
Paula McMaw <paula@puppet.com>
2017-06-30 11:33:32 +0100

    Addressing Rubocop Errors
    There are rubocop errors that result in Travis failures, I am correcting
    them.

--- lib/string_prep/string_wrapper.rb
+++ lib/string_prep/string_wrapper.rb
@@ -13,54 +13,53 @@
 class StringWrapper < Parser::Rewriter
   def on_dstr(node)
     if node.loc.respond_to?(:heredoc_body)
-      insert_before(node.loc.expression, "_(")
-      insert_after(node.loc.expression, ")")
+      insert_before(node.loc.expression, '_(')
+      insert_after(node.loc.expression, ')')
     elsif node.loc.respond_to?(:begin)
-      insert_before(node.loc.begin, "_(")
-      insert_after(node.loc.end, ")")
+      insert_before(node.loc.begin, '_(')
+      insert_after(node.loc.end, ')')
     end
   end
 
   def on_str(node)
     if node.loc.respond_to?(:heredoc_body)
-      insert_before(node.loc.expression, "_(")
-      insert_after(node.loc.expression, ")")
+      insert_before(node.loc.expression, '_(')
+      insert_after(node.loc.expression, ')')
     elsif node.loc.respond_to?(:begin)
       # avoid constants, like __FILE__, which do not have begin/end nodes and should not be marked
       str = node.children[0]
-      return if str == ""         # ignore empty strings
+      return if str == ''         # ignore empty strings
       return if str !~ /[A-Za-z]/ # ignore non-text strings
       return if str =~ /^\.*\//   # ignore many file paths
-      #ignore strings with no whitespace - are typically keywords or interpolation statements and cover the above commented-out statements
+      # ignore strings with no whitespace - are typically keywords or interpolation statements and cover the above commented-out statements
       return if str =~ /^\S*$/
-      insert_before(node.loc.begin, "_(")
-      insert_after(node.loc.end, ")")
+      insert_before(node.loc.begin, '_(')
+      insert_after(node.loc.end, ')')
     end
   end
 
   def on_send(node)
     method_name = node.loc.selector.source
-    return unless /call_function|function_deprecation|new(param|function|property)|raise|desc|\+|.*[Ee]rror|warning|warn|fail/.match(method_name)
+    return unless /call_function|function_deprecation|new(param|function|property)|raise|desc|\+|.*[Ee]rror|warning|warn|fail/=~method_name
     if method_name == 'raise'
       _receiver_node, _method_name, *arg_nodes = *node
       if !arg_nodes.empty? && arg_nodes[0].type == :const
         # skip errors that are only logged in debug mode
-        return if arg_nodes[0].loc.name.source == "DevError"
+        return if arg_nodes[0].loc.name.source == 'DevError'
       end
     end
     super
   end
 
-  def on_regexp(node)
-    return
+  def on_regexp(_node)
+    nil
   end
 
-  def on_array(node)
-    return
+  def on_array(_node)
+    nil
   end
 
-  def on_xstr(node)
-    return
+  def on_xstr(_node)
+    nil
   end
 end
-
